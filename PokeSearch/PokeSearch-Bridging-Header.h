79c875eb86633967d42d30357846f164266bf501
Mina Guirguis <MGuirguis.10@gmail.com>
2018-03-08 00:41:59 -0500

    Install Geofire and Firebase

---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeSearch-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by Mina Guirguis on 3/8/18.
+//  Copyright © 2018 Mina Guirguis. All rights reserved.
+//
+
+#import "GeoFire.h"

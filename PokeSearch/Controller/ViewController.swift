a52c9d3567e97ca977f9651e8c8474c157b061f6
Mina Guirguis <MGuirguis.10@gmail.com>
2018-03-24 21:43:13 -0400

    Added a function to customize annotation and mapView

--- PokeSearch/Controller/ViewController.swift
+++ PokeSearch/Controller/ViewController.swift
@@ -69,12 +69,35 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
+        let annoIdentifier = "Pokemon"
         var annotationView: MKAnnotationView?
         
         if annotation.isKind(of: MKUserLocation.self) {
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
             annotationView?.image = UIImage(named: "ash")
+        } else if let deqAnno = mapView.dequeueReusableAnnotationView(withIdentifier: annoIdentifier) {
+            annotationView = deqAnno
+            annotationView?.annotation = annotation
+        } else {
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: annoIdentifier)
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = av
+        }//we need this "else" statment here in case this deque fails, and we need to create a default annotation
+        
+        if let annotationView = annotationView, let anno = annotation as? PokeAnnotation{
+            
+            annotationView.canShowCallout = true//if you show a callout for an annotation you have to set the title
+            annotationView.image = UIImage(named: "\(anno.pokemonNumber)")
+            //this line above code works greate because our pokemon pics are named as numbers
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            btn.setImage(UIImage(named: "map"), for: .normal)
+            annotationView.rightCalloutAccessoryView = btn
+            
         }
+        //the if statment above is making sure that the annotation view is not nil
+        //now if it is a default annotation we are going to go in and customize it
+        //also this if statment is making sure we can successfully cast this annotation as a pokeAnnotation
         
         return annotationView
     }
@@ -96,6 +119,30 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             }
         })
 }
+    
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        showSightingsOnMap(location: loc)
+    }
+    //this function is in place if the user decides to zoom out or in on the mapview
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if let anno = view.annotation as? PokeAnnotation {
+            let place = MKPlacemark(coordinate: anno.coordinate)
+            let destination = MKMapItem(placemark: place)
+            destination.name = "PokemonSighting"
+            let regionDistance: CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(anno.coordinate, regionDistance, regionDistance)
+            
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] as [String : Any]
+            //we want to be in the center of the region and span out, and to make sure we show driving directions
+            
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+            //this should open up maps and pass the information
+        }
+    }
+    //function above is configuring map before we open it
     
 
     @IBAction func spotRandomPokemon(_ sender: Any) {
